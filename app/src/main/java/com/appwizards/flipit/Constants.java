ace5c2b8f836cf3cad15b27f0dbe2ba586432edc
ukashanoor <ukasha.noor@bigyellowfish.io>
2023-07-12 16:45:26 +0100

    Added the App

---
+++ app/src/main/java/com/appwizards/flipit/Constants.java
@@ -0,0 +1,14 @@
+package com.appwizards.flipit;
+
+public final class Constants {
+    public static final String PREF_NAME = "HighScore";
+    public static final String EASY_HIGH_KEY = "easy_high";
+    public static final String HARD_HIGH_KEY = "hard_high";
+    public static final int LEVEL_EASY = 0;
+    public static final int LEVEL_HARD = 1;
+    public static final int HARD_NO_OF_CARDS = 16;
+    public static final int EASY_NO_OF_CARDS = 12;
+    public static final long EASY_TIME = 220000;
+    public static final long HARD_TIME = 320000;
+    public static final int TIMER_INTERVAL = 1000;
+}
