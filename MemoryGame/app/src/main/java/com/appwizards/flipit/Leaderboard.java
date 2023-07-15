019044cc2c27058350bf33befe3c1a4792994a99
ukashanoor <ukasha.noor@bigyellowfish.io>
2023-07-15 16:35:37 +0100

    added changes for background and leaderboard

--- MemoryGame/app/src/main/java/com/appwizards/flipit/Leaderboard.java
+++ MemoryGame/app/src/main/java/com/appwizards/flipit/Leaderboard.java
@@ -1,17 +1,23 @@
 package com.appwizards.flipit;
 
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class Leaderboard extends Fragment {
-
+    private static String[] dataList = {"Noor", "Folk", "Yogesh"};
     public Leaderboard() {}
 
     @Override
@@ -21,6 +27,30 @@ public class Leaderboard extends Fragment {
         SharedPreferences preferences = getActivity().getSharedPreferences(Constants.PREF_NAME,0);
         ((TextView) rootView.findViewById(R.id.easylead)).append(""+preferences.getInt(Constants.EASY_HIGH_KEY, (int) (Constants.EASY_TIME/Constants.TIMER_INTERVAL)));
         ((TextView) rootView.findViewById(R.id.hardlead)).append(""+preferences.getInt(Constants.HARD_HIGH_KEY, (int) (Constants.HARD_TIME/Constants.TIMER_INTERVAL)));
+        LinearLayout containerLayout = rootView.findViewById(R.id.llLeaderboard);
+        Context context = getActivity();
+        MyDBHelper dbHelper = new MyDBHelper(context);
+        ArrayList<MyDBHelper.MyData> score = dbHelper.getScore();
+        for (MyDBHelper.MyData data : score) {
+            LinearLayout horizontalLayout = new LinearLayout(context);
+            horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+            horizontalLayout.setPadding(10, 10, 10, 10);
+
+            TextView textView1 = new TextView(context);
+            textView1.setText(data.getName());
+            textView1.setPadding(30, 10, 10, 10);
+            textView1.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.START));
+
+            TextView textView2 = new TextView(context);
+            textView2.setText(data.getScore());
+            textView2.setPadding(100, 10, 0, 10);
+            textView2.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.END));
+
+            horizontalLayout.addView(textView1);
+            horizontalLayout.addView(textView2);
+
+            containerLayout.addView(horizontalLayout);
+        }
         return rootView;
     }
 
--- MemoryGame/app/src/main/java/com/appwizards/flipit/MyDBHelper.java
+++ MemoryGame/app/src/main/java/com/appwizards/flipit/MyDBHelper.java
@@ -2,13 +2,42 @@ package com.appwizards.flipit;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MyDBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "GameDB";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_LEADERBOARD = "leaderboard";
+
+    public class MyData {
+        private int id;
+        private String name;
+        private String score;
+
+        public MyData(int id, String name, String score) {
+            this.id = id;
+            this.name = name;
+            this.score = score;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getScore() {
+            return score;
+        }
+    }
 
     public MyDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -33,5 +62,28 @@ public class MyDBHelper extends SQLiteOpenHelper {
         contentValues.put("name", name);
         contentValues.put("time", time);
         db.insert(TABLE_LEADERBOARD, null, contentValues);
+    }
+
+    public ArrayList<MyData> getScore() {
+        List<MyData> dataList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_LEADERBOARD, null);
+        if (cursor.moveToFirst()) {
+            do {
+                // Extract data from the cursor
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                String score = cursor.getString(cursor.getColumnIndex("time"));
+
+                // Create a MyData object and add it to the list
+                MyData data = new MyData(id, name, score);
+                dataList.add(data);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+
+        return (ArrayList<MyData>) dataList;
     }
 }
